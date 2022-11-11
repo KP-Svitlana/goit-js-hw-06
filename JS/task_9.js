db003ee0bdf2ed76e289ebbf1d1c830c97661fbb
KP-Svitlana <kovalenko.svitlana.p@gmail.com>
2022-11-11 16:04:16 +0200

    added Task_9

---
+++ JS/task_9.js
@@ -0,0 +1,18 @@
+const button = document.querySelector(".change-color");
+const span = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener("click", getChangeBackgrounColor);
+
+function getChangeBackgrounColor(event) {
+  let color = getRandomHexColor();
+
+  body.style.backgroundColor = `${color}`;
+  span.textContent = `${color}`;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
